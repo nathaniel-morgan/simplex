b8ad2e193d9853bab04ffd7c910880e4bbbc66e6
nathaniel-morgan <nathaniel.morgan1999@gmail.com>
2025-09-12 13:27:35 -0500

    name method completed

--- R/boxplot2.R
+++ R/boxplot2.R
@@ -67,12 +67,12 @@ make_template <- function(fun = NULL, template_name = "basic"){
   }
 }
 
-#' Basic boxplot plotter.  Templates can be made to adjust it with make_template.
+#' Basic boxplot plotter.  code can be edited before plotting with format edit. boxplot2_adjust can run the modified code.
 #'
 #' @param x data in the form of a dbl vecotor for which you would like the make a boxplot
 #' @param format Whether the function should plot, or return an object for editing.
 #'
-#' @return a ggplot object
+#' @return a ggplot object or a list containing ggplot code along with a function environment.
 #'
 #' @examples
 #' a <- rbeta(1e2,1,3)
@@ -116,6 +116,56 @@ boxplot2_original <- function(
 }
 
 #' @export
-boxplot2_adjust <- function(){
-  eval(code_output$code, envir = code_output$env)
+boxplot2_adjust <- function(name = "code_output"){
+  eval(get(name)$code, envir = get(name)$env)
+}
+
+#' Basic boxplot plotter. code can be modified before plotting with format edit
+#'
+#' @param x data in the form of a dbl vecotor for which you would like the make a boxplot
+#' @param format Whether the function should plot, or return an object for editing.
+#' @param name what name will the list object containing your plot code and environment contain
+#'
+#' @return a ggplot object or a named list containing ggplot code along with a function environment.
+#'
+#' @examples
+#' a <- rbeta(1e2,1,3)
+#' boxplot2_name(x = a) # just makes a boxplot
+#' boxplot2_name(x = a, formate = "edit", name = "circle_points") # opens a doc for editing
+#' boxplot2_adjust("circle_points") #runs the edited code
+#'
+#' @export
+#'
+boxplot2_name <- function(
+    x,
+    format = "plot",
+    name = "code_output"
+) {
+  mean_x <- mean(x)
+  sd_x <- sd(x)
+  outlier_range <- sd_x*2.5
+  outlier <- data.frame(x = x[x-outlier_range>0|x+outlier_range<0])
+  x_trim <- x[x-outlier_range<0&x+outlier_range>0]
+  qx <- quantile(x_trim, probs = c(0, .25, .5, .75, 1))
+  x_trim <- data.frame(x = x_trim)
+
+  assign(name,quote(
+    ggplot(outlier, aes(x = 0))+
+      geom_point(aes(y = x), pch = 8)+
+      geom_errorbar(ymin = qx[1], ymax = qx[5], width = 0.1)+
+      geom_crossbar(ymin = qx[2], y = qx[3], ymax = qx[4], width = 0.2, fill = "white")+
+      expand_limits(y = range(x))))
+  if (format == "plot") eval(code_output)
+  else if (format == "edit") {
+    output <- glue("#Could concievably put an explanation of how to edit the code here \n\n",
+                   name,"$code <- ","expr({{",
+                   system.file("extdata", "boxplot2.txt", package = "simplex") |>
+                     readLines() |> paste(collapse = "\n"),"}})")
+    rstudioapi::documentNew(
+      text = output,
+      type = "r"
+    )
+    assign(name, list(code = code_output, env = environment()), envir = .GlobalEnv)
+  }
+  else list(code = get(name), env = environment())
 }
